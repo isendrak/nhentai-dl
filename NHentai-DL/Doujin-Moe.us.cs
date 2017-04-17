0b9fc82c3b61e6c0e62ac48bfb746b37d242c91c
Isendrak Skatasmid <Isendrak@googlemail.com>
2017-04-17 20:51:28 +0200

    fixed doujin-moe.us/doujins.com plugin (again...)

--- NHentai-DL/Doujin-Moe.us.cs
+++ NHentai-DL/Doujin-Moe.us.cs
@@ -16,11 +16,10 @@ namespace NHentaiDL{
 				GalleryUrl = "http://"+GalleryUrl;
 			string Body = wc.DownloadString(GalleryUrl);
 			string Title = Regex.Match(Body, "<title>(?:Doujin-moe|Doujins|Doujins\\.com)[ \t]*-[ \t]*([^<]+)</title>").Groups[1].Value;
-			Console.WriteLine(Title);
 			int Page = 1;
-			foreach(Match match in Regex.Matches(Body, "file=\"http://(static\\.doujin-moe\\.us|static[0-9]*\\.doujins\\.com)/r-([^\\.]+)\\.(jpg|png|bmp|jpeg|gif)\\?st=([^&]+)&e=([0-9]+)\"")){
-				if(match.Groups.Count != 6) continue;
-				Images.Add(new ImageInfo{ Filename = string.Format("{0:0000}.{1}", Page, match.Groups[3].Value), URL = new Uri(string.Format("http://static.doujin-moe.us/r-{0}.{1}?st={2}&e={3}", match.Groups[2].Value, match.Groups[3].Value, match.Groups[4].Value, match.Groups[5].Value)) });
+			foreach(Match match in Regex.Matches(Body, "file=\"(http[s]{0,1})://(static\\.doujin-moe\\.us|static[0-9]*\\.doujins\\.com)/r-([^\\.]+)\\.(jpg|png|bmp|jpeg|gif)\\?st=([^&]+)&e=([0-9]+)\"")){
+				if(match.Groups.Count != 7) continue;
+				Images.Add(new ImageInfo{ Filename = string.Format("{0:0000}.{1}", Page, match.Groups[3].Value), URL = new Uri(string.Format("{0}://{1}/r-{2}.{3}?st={4}&e={5}", match.Groups[1], Regex.Replace(match.Groups[2].Value,"static[0-9]+\\.","static."), match.Groups[3].Value, match.Groups[4].Value, match.Groups[5].Value, match.Groups[6])) });
 				Page++;
 			}
 			return new GalleryInfo{Name = Title, Images = Images };
